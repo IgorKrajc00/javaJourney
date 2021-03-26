759e07e956071c48df46cc52633784405567a257
IgorKrajc00 <71592418+IgorKrajc00@users.noreply.github.com>
2021-03-26 18:27:11 +0100

    added 'id' and made class neater

--- EmployeeTest/src/Employee.java
+++ EmployeeTest/src/Employee.java
@@ -1,22 +1,50 @@
 import java.time.LocalDate;
+import java.util.Objects;
 
 class Employee {
-    private String name;
-    private double salary;
-    private LocalDate hireDay;
 
+    // instance fields of Employee class
+    private final String name;
+    private double salary;
+    private final LocalDate hireDay;
+
+    private static int nextId = 1;
+    private int id;
+
+    // Employee constructor
     public Employee(String n, double s, int year, int month, int day) {
+        Objects.requireNonNull(n, "The name cannot be null");   // secures that n != NULL
+        // similar logic to the line above -> if (n == null) this.name = "unknown"; else this.name = n;
         this.name = n;
         this.salary = s;
         this.hireDay = LocalDate.of(year, month, day);
     }
 
-    public String getName() { return name; }
-    public double getSalary() { return salary; }
-    public LocalDate getHireDay() { return hireDay; }
+    // accessor methods of Employee class
+    public String getName() {
+        return name;
+    }
 
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // method that raises salary by 'byPercent'%
     public void raiseSalary(double byPercent) {
-        double raise = salary * byPercent / 100;
-        salary += raise;
+        double raise = this.salary * byPercent / 100;
+        this.salary += raise;
+    }
+
+    public void setId() {
+        this.id = nextId;
+        nextId++;
     }
 }
